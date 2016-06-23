b8a584ca34a6044b29457472e0ca9e144a545078
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2016-06-23 12:09:24 +0530

    jsdom support

--- mocha_helper.js
+++ mocha_helper.js
@@ -2,10 +2,20 @@ var jsdom = require('jsdom');
 var React = require('react');
 var sinon = require('sinon');
 
-require('babel-core/register');
+
 global.document = jsdom.jsdom('<!doctype html><html><body> <div id="app"></div></body></html>');
 global.window = document.defaultView;
-global.navigator = {userAgent: 'node.js'};
+
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+  	//setting more globals here
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
 
 //Set React global only after setting document, window
 var React = require('react');
